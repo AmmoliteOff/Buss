d58c222609b5c8b705351ba6f9544486d41c3f1a
nakon <nakonectlichika@mail.ru>
2023-11-24 20:29:15 +0300

    Initial commit

---
+++ src/test/java/org/hackathon/buss/BussApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hackathon.buss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
