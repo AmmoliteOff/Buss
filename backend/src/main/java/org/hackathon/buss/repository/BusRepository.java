684683a73d879868c6eea4f5573ac92cbfebd32c
DimaKotovProgrammist <dskotov003@gmail.com>
2023-11-25 10:45:39 +0300

    minor changes

--- backend/src/main/java/org/hackathon/buss/repository/BusRepository.java
+++ backend/src/main/java/org/hackathon/buss/repository/BusRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BusRepository extends JpaRepository<Long, Bus>{
+public interface BusRepository extends JpaRepository<Bus, Long>{
 }
