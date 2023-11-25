cde0e242007296c69eb76a6e3e6b9172ea2265f2
DimaKotovProgrammist <dskotov003@gmail.com>
2023-11-25 23:57:56 +0300

    added methods to BusService and their use in BusController

--- backend/src/main/java/org/hackathon/buss/controller/BusController.java
+++ backend/src/main/java/org/hackathon/buss/controller/BusController.java
@@ -4,20 +4,37 @@ import lombok.RequiredArgsConstructor;
 import org.hackathon.buss.model.Bus;
 import org.hackathon.buss.service.BusService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/bus")
+@RequestMapping("/api/buses")
 public class BusController {
 
     private final BusService busService;
 
-    @GetMapping()
-    public ResponseEntity<Bus> getBus(long busId) {
-        Bus bus = busService.findBus(busId).orElse(null);
+    @GetMapping("/{id}")
+    public ResponseEntity<Bus> getBus(@PathVariable Long id) {
+        Bus bus = busService.findById(id).orElse(null);
         return ResponseEntity.ok(bus);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Bus>> getAllBuses() {
+        List<Bus> buses = busService.findAll();
+        return ResponseEntity.ok(buses);
+    }
+
+    @PostMapping()
+    public  ResponseEntity<Bus> addBus(@RequestBody Bus bus) {
+        return ResponseEntity.ok(busService.save(bus));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Bus> deleteBus(@PathVariable Long id) {
+        busService.delete(id);
+        return ResponseEntity.ok(new Bus());
     }
 }
--- backend/src/main/java/org/hackathon/buss/service/BusService.java
+++ backend/src/main/java/org/hackathon/buss/service/BusService.java
@@ -5,6 +5,7 @@ import org.hackathon.buss.model.Bus;
 import org.hackathon.buss.repository.BusRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,7 +14,19 @@ public class BusService {
 
     private final BusRepository busRepository;
 
-    public Optional<Bus> findBus(long id) {
+    public Optional<Bus> findById(long id) {
         return busRepository.findById(id);
+    }
+
+    public List<Bus> findAll() {
+        return busRepository.findAll();
+    }
+
+    public Bus save(Bus bus) {
+        return busRepository.save(bus);
+    }
+
+    public void delete(Long id) {
+        busRepository.delete(findById(id).orElseThrow());
     }
 }
