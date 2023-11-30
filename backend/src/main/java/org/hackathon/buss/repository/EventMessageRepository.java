3883bff0b75ffbe8cce9878a6d642737a994c809
DimaKotovProgrammist <dskotov003@gmail.com>
2023-11-30 15:09:32 +0300

    classes removed

--- backend/src/main/java/org/hackathon/buss/repository/EventMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.hackathon.buss.repository;
-
-import org.hackathon.buss.model.EventMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventMessageRepository extends JpaRepository<EventMessage, Long> {
-}
