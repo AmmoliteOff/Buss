c2e0d6fd8483da75066a973e7b87859e1ee7e8c9
DimaKotovProgrammist <dskotov003@gmail.com>
2023-12-01 10:37:54 +0300

    added configuration for swagger

--- backend/src/main/java/org/hackathon/buss/BussApplication.java
+++ backend/src/main/java/org/hackathon/buss/BussApplication.java
@@ -1,9 +1,15 @@
 package org.hackathon.buss;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Buss api")
+)
 public class BussApplication {
 
     public static void main(String[] args) {
