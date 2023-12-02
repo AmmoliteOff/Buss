f1597153f1474df9948ea6f944251c4ec37407e9
DimaKotovProgrammist <dskotov003@gmail.com>
2023-12-02 18:46:25 +0300

    added registerEvent method and EventRegistrationListener class

--- backend/src/main/java/org/hackathon/buss/controller/UserController.java
+++ backend/src/main/java/org/hackathon/buss/controller/UserController.java
@@ -3,18 +3,31 @@ package org.hackathon.buss.controller;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 
+import org.hackathon.buss.model.Event;
 import org.hackathon.buss.model.User;
 import org.hackathon.buss.service.UserService;
 import org.hackathon.buss.util.view.DetailedInformation;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping(value = "/api/users",
+                produces = MediaType.APPLICATION_JSON_VALUE,
+                consumes = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
     private final UserService userService;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @PostMapping("/register-event")
+    public ResponseEntity<String> registerEvent(@RequestBody Event event) {
+        rabbitTemplate.convertAndSend( "",  "event-registration", event);
+        return ResponseEntity.ok("Ok");
+    }
 
     @GetMapping("/{id}")
     @JsonView({DetailedInformation.class})
