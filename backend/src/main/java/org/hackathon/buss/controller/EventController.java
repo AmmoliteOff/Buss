545b344d5f508fcb1d2f0405ed8198ce6a729540
DimaKotovProgrammist <dskotov003@gmail.com>
2023-12-02 18:46:56 +0300

    changed the logic

--- backend/src/main/java/org/hackathon/buss/controller/EventController.java
+++ backend/src/main/java/org/hackathon/buss/controller/EventController.java
@@ -2,17 +2,11 @@ package org.hackathon.buss.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
-import org.hackathon.buss.enums.Role;
-import org.hackathon.buss.model.Dispatcher;
 import org.hackathon.buss.model.Event;
 import org.hackathon.buss.service.EventService;
-import org.hackathon.buss.service.UserService;
 import org.hackathon.buss.util.view.NonDetailedInformation;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,23 +15,8 @@ public class EventController {
 
     private final EventService eventService;
 
-    private final UserService userService;
-
-    @PostMapping("/report")
-    @JsonView({NonDetailedInformation.class})
-    public ResponseEntity<?> reportAnEvent (@RequestBody Event event) {
-        if(event.getDriver().getRole().equals(Role.DRIVER)) {
-           Dispatcher dispatcher = userService.findMinLoadedDispatcher();
-            event.setDateTime(LocalDateTime.now());
-            dispatcher.getEvents().add(event);
-            event.setDispatcher(dispatcher);
-            return ResponseEntity.ok(eventService.save(event));
-        } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You don't have enough rights");
-        }
-    }
-
     @PatchMapping("/process")
+    @JsonView(NonDetailedInformation.class)
     public ResponseEntity<?> processAnEvent (@RequestBody Event event) {
         Event processedEvent = eventService.findById(event.getId()).orElseThrow();
         processedEvent.setProcessed(true);
--- backend/src/main/java/org/hackathon/buss/service/EventService.java
+++ backend/src/main/java/org/hackathon/buss/service/EventService.java
@@ -1,6 +1,7 @@
 package org.hackathon.buss.service;
 
 import lombok.AllArgsConstructor;
+import org.hackathon.buss.model.Dispatcher;
 import org.hackathon.buss.model.Event;
 import org.hackathon.buss.repository.EventRepository;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,18 @@ public class EventService {
 
     private final EventRepository eventRepository;
 
+    private final UserService userService;
+
     public Optional<Event> findById(Long id) {
         return eventRepository.findById(id);
     }
 
     public Event save(Event event) {
         if(event.getId() == null) {
+            Dispatcher dispatcher = userService.findMinLoadedDispatcher();
+            event.setDateTime(LocalDateTime.now());
+            dispatcher.getEvents().add(event);
+            event.setDispatcher(dispatcher);
             event.setDateTime(LocalDateTime.now());
         }
         return eventRepository.save(event);
