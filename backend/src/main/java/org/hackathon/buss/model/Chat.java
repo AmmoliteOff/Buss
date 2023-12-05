3928359e6808f1416c352f2bdcf8c6fb92bd2e32
DimaKotovProgrammist <dskotov003@gmail.com>
2023-12-05 22:35:12 +0300

    added class Chat

---
+++ backend/src/main/java/org/hackathon/buss/model/Chat.java
@@ -0,0 +1,24 @@
+package org.hackathon.buss.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "chats")
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany
+    private List<Message> messages;
+}
