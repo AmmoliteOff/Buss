174fd57c21219b069ec0c49f2caef0e7e6dd3b29
DimaKotovProgrammist <dskotov003@gmail.com>
2023-12-06 16:36:19 +0300

    I'm tired of working as a farmhand

--- backend/src/main/java/org/hackathon/buss/service/MessageService.java
+++ backend/src/main/java/org/hackathon/buss/service/MessageService.java
@@ -1,6 +1,7 @@
 package org.hackathon.buss.service;
 
 import lombok.AllArgsConstructor;
+import org.hackathon.buss.model.Driver;
 import org.hackathon.buss.model.Message;
 import org.hackathon.buss.repository.MessageRepository;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,13 @@ import java.time.LocalDateTime;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final UserService userService;
+    private final ChatService chatService;
 
     public Message save(Message message){
+        Driver driver = new Driver(userService.findById(message.getSender().getId()).orElseThrow());
         message.setSendAt(LocalDateTime.now());
+        message.setChat(chatService.findByDriver(driver).orElseThrow());
         return messageRepository.save(message);
     }
 }
