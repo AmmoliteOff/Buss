d8cf7aee92c33332f500cd0eaf83f89bb170a111
nakon <nakonectlichika@mail.ru>
2023-12-06 10:55:33 +0300

    Real Time Update v2

--- backend/src/main/java/org/hackathon/buss/repository/ScheduleEntryReposirory.java
+++
@@ -1,7 +0,0 @@
-package org.hackathon.buss.repository;
-
-import org.hackathon.buss.model.ScheduleEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleEntryReposirory extends JpaRepository<ScheduleEntry, Long> {
-}
