fc327e902efb3668156dde2a1069f4565d6c8889
nakon <nakonectlichika@mail.ru>
2023-12-07 04:35:02 +0300

    Fixes

--- backend/src/main/java/org/hackathon/buss/repository/RoadStopsRepository.java
+++ backend/src/main/java/org/hackathon/buss/repository/RoadStopsRepository.java
@@ -1,9 +1,13 @@
 package org.hackathon.buss.repository;
 
+import org.hackathon.buss.model.Bus;
 import org.hackathon.buss.model.RoadStops;
 import org.hackathon.buss.model.Stop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoadStopsRepository extends JpaRepository<RoadStops, Long> {
     public RoadStops findByStop(Stop stop);
+    public List<RoadStops> findAllByBus(Bus bus);
 }
