d78abe27f50ac82e2b10d8fff54b35f68bdd06e9
DimaKotovProgrammist <dskotov003@gmail.com>
2023-12-07 05:20:33 +0300

    added @JsonView

--- backend/src/main/java/org/hackathon/buss/model/stats/RouteStatsByInterval.java
+++ backend/src/main/java/org/hackathon/buss/model/stats/RouteStatsByInterval.java
@@ -1,17 +1,22 @@
 package org.hackathon.buss.model.stats;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import lombok.Data;
-import org.hackathon.buss.model.Stop;
+import org.hackathon.buss.util.view.DetailedInformation;
+import org.hackathon.buss.util.view.NonDetailedInformation;
 
 @Entity
 @Data
+@JsonView(NonDetailedInformation.class)
 public class RouteStatsByInterval {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(DetailedInformation.class)
     private Long routeStatsByIntervalId;
 
     @ManyToOne
+    @JsonView(DetailedInformation.class)
     private RouteStatsByStop routeStatsByStop;
 
     private int totalPeopleCount;
